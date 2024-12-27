a6afaac5fedb17b09a72085c72d31a10ed46329a
anouaramghar <anwaramghar888@gmail.com>
2024-12-27 14:48:21 +0100

    2

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/recipes-website/',
   server: {
     host: "::",
     port: 8080,
@@ -18,4 +19,4 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+}))
